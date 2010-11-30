32560577944896f2df16302d2234edd8fa262c7a
jrd <jrd@seamcity.enialis.net>
2010-11-30 16:38:07 +0100

    indentation et affichage correct du texte centré.

--- countdown.py
+++ countdown.py
@@ -1,5 +1,5 @@
-# !/usr/bin/env python2
-# coding: utf8
+# !/usr/bin/env python
+# -*- coding: utf8 -*-
 
 ############################################################################
 # Lua countdown python version                                             #
@@ -30,78 +30,84 @@ h = 100
 step = 10
 
 class Countdown(gtk.DrawingArea):
-    def __init__(self):
-        gtk.DrawingArea.__init__(self)
-        self.connect("expose_event", self.expose)
-        self.inc = 100
-        self.val = 99
-        
-    def expose(self, widget, event):
-        self.context = widget.window.cairo_create()
-        self.draw(self.context)
-        return False
-    
-    def draw(self, context):
-	context.set_line_width(3)
-	self.inc = self.inc - 1
-	if self.inc == 0:
-            self.inc = 100
-            self.val = self.val - 1
-            if self.val == 0:
-                gtk.main_quit()
+  def __init__(self):
+    gtk.DrawingArea.__init__(self)
+    self.connect("expose_event", self.expose)
+    self.inc = 100
+    self.val = 19
 
-        context.set_source_rgba(1.0, 1.0, 1.0, 0.0)   
-        context.set_operator(cairo.OPERATOR_SOURCE)
-        context.paint()
+  def expose(self, widget, event):
+    self.context = widget.window.cairo_create()
+    self.draw(self.context)
+    return False
 
-        context.set_source_rgba(0, 0, 0, 1)
-	context.arc(50, 50, 40, 0, 2 * math.pi)
-	context.fill()
+  def draw(self, context):
+    context.set_line_width(3)
+    self.inc = self.inc - 1
+    if self.inc == 0:
+      self.inc = 100
+      self.val = self.val - 1
+      if self.val == 0:
+        gtk.main_quit()
 
-	context.set_source_rgba(0, 0.4, 1, 0.8)
-	context.arc(50, 50, 40, 0, 2 * math.pi)
-	context.stroke()
+    context.set_source_rgba(1, 1, 1, 0)
+    context.set_operator(cairo.OPERATOR_SOURCE)
+    context.paint()
 
-	context.set_source_rgba(0.2, 0.8, 1, 0.8)
-	context.arc(50, 50, 40, 0, 2 * math.pi * self.inc / 100 )
-	context.stroke()
+    context.set_source_rgba(0, 0, 0, 1)
+    context.arc(50, 50, 40, 0, 2 * math.pi)
+    context.fill()
 
-	context.move_to(50 - (25 * (len(str(self.val)) / 2)), 65)
-	context.set_font_size(40)
-	context.show_text(str(self.val))
+    context.set_source_rgba(0, 0.4, 1, 0.8)
+    context.arc(50, 50, 40, 0, 2 * math.pi)
+    context.stroke()
+
+    context.set_source_rgba(0.2, 0.8, 1, 0.8)
+    context.arc(50, 50, 40, 0, 2 * math.pi * self.inc / 100 )
+    context.stroke()
+
+    context.move_to(50 - (25 * len(str(self.val)) / 2), 65)
+    context.set_font_size(40)
+    context.show_text(str(self.val))
 
 def timeout(cd):
-    cd.queue_draw()
-    return True
+  cd.queue_draw()
+  return True
 
 def transparent_expose(widget, event):
-	cr = widget.window.cairo_create()
-	cr.set_operator(cairo.OPERATOR_CLEAR)
-	region = gtk.gdk.region_rectangle(event.area)
-	cr.region(region)
-	cr.fill()
-	return False
+  cr = widget.window.cairo_create()
+  cr.set_operator(cairo.OPERATOR_CLEAR)
+  region = gtk.gdk.region_rectangle(event.area)
+  cr.region(region)
+  cr.fill()
+  return False
 
 def main():
-    window = gtk.Window()
-    cd = Countdown()
-    window.add(cd)
-    window.set_title("Countdown Widget")
-    window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)
-    window.set_keep_below(True)
-    window.set_decorated(False)
-    window.stick()
-    window.set_position(gtk.WIN_POS_CENTER)
-    window.set_colormap(window.get_screen().get_rgba_colormap())
-    window.set_app_paintable(True)
-    window.connect("expose-event", transparent_expose)
-    window.set_size_request(w, h)
-    window.set_double_buffered(True)
-    window.connect("destroy", gtk.main_quit)
-    window.show_all()
-    gobject.timeout_add(step, timeout, cd)
-    gtk.main()
+  window = gtk.Window()
+  cd = Countdown()
+  window.add(cd)
+  window.set_title("Countdown Widget")
+  window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)
+  window.set_skip_taskbar_hint(True)
+  window.set_skip_pager_hint(True)
+  window.set_keep_below(True)
+  window.set_decorated(False)
+  window.stick()
+  window.set_position(gtk.WIN_POS_CENTER)
+  colormap = window.get_screen().get_rgba_colormap()
+  if colormap == None:
+    print 'Your screen does not support alpha channels.'
+    colormap = window.get_screen().get_rgb_colormap()
+  window.set_colormap(colormap)
+  window.set_app_paintable(True)
+  window.connect("expose-event", transparent_expose)
+  window.set_size_request(w, h)
+  window.set_double_buffered(True)
+  window.connect("destroy", gtk.main_quit)
+  window.show_all()
+  gobject.timeout_add(step, timeout, cd)
+  gtk.main()
 
-    
+
 if __name__ == "__main__":
-    main()
+  main()
